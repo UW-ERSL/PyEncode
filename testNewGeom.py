2ecdc48bac93263905f2db181d71d45b6e3ca857
UW-ERSL <ksuresh@wisc.edu>
2026-04-20 12:07:34 -0500

    Deleted old files

--- testNewGeom.py
+++
@@ -1,3 +0,0 @@
-from pyencode import encode, GEOMETRIC
-circuit, info = encode(GEOMETRIC(ratio=0.95, start=32), N=64)
-print(f'✓ GEOMETRIC start parameter working: {info.gate_count_1q} gates, depth {info.circuit_depth}')
